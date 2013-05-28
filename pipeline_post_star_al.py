476fb580418ebe359747d7e33b55276bf60f4c53
Thies Gehrmann <t.gehrmann@tudelft.nl>
2013-05-28 08:54:16 +0200

    Split STAR_AL into STAR_AL and POST_STAR_AL

---
+++ pipeline_post_star_al.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+import os;
+import sys;
+from pipeline_common import *;
+
+###############################################################################
+
+def usage(a1):
+  print "Usage:  %s <config file>" % a1;
+#edef
+
+if len(os.sys.argv) != 2:
+  usage(os.sys.argv[0]);
+  os.sys.exit(1);
+#fi
+
+C = PIPELINECONF(os.sys.argv[1]);
+run_cmd('mkdir -p %s' % C.outdir);
+
+###############################################################################
+
+ALN = C.star_al_output();
+
+for i in xrange(len(ALN)):
+  a  = ALN[i];
+  sn = C.sample_names[i];
+
+  nfname = '%s/%s.star_align.bam' % (C.outdir, sn);
+  sfpref = '%s/%s.star_align_sort_name' % (C.outdir, sn);
+
+  cmds = [];
+
+  cmds.append("samtools view -Sb '%s' -o '%s'" % (a, nfname));
+  cmds.append("samtools sort -n -m 100000000000 '%s' '%s'" % (nfname, sfpref));
+
+#efor
+
+retval = run_seq_cmds(cmds);
+if retval != 0:
+  sys.exit(retval);
+#fi
+
+sys.exit(0);
+
