385b34fd4932351dd5f1a11c070dae5db68ed576
Thies Gehrmann <t.gehrmann@tudelft.nl>
2013-06-05 10:59:32 +0200

    GFF check, CUFFLINKS now running, status update

---
+++ pipeline_genome_annot_format.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import os;
+import sys;
+
+from pipeline_common import *;
+
+###############################################################################
+
+def usage(a1):
+  print "Usage:  %s <config file>" % a1;
+#edef
+
+if len(os.sys.argv) != 2:
+  usage(os.sys.argv[0]);
+  os.sys.exit(1);
+#fi
+
+C = PIPELINECONF(os.sys.argv[1]);
+run_cmd('mkdir -p %s' % C.outdir);
+
+###############################################################################
+
+gffout = C.__genome_annot_format_output__();
+
+cmd = 'gffread ' \
+    + '-E %s ' % C.genome_annot \
+    + '-o %s'  % gffout;
+
+retval = run_cmd(cmd);
+
+sys.exit(retval);
