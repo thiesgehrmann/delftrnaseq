b26c955c8136de505c4ee294c5dd8facaf7dd8a7
Marc Hulsman <m.hulsman@tudelft.nl>
2013-11-15 16:15:12 +0100

    update conf

--- pipeline_conf.py
+++ pipeline_conf.py
@@ -4,45 +4,62 @@ class config(PIPELINECONF):
   #inst_loc
 
   email   = "m.hulsman@tudelft.nl";
-  jobname = "FENGFENG";
+  jobname = "RJTIME";
 
   location = "./conf";
   workdir = None;
-  outdir  = "/data/tmp/marchulsman/run_feng"
+  outdir  = "/data/tmp/marchulsman/run_rj"
   #makefile="";
   blast_db = '/data/tmp/marchulsman/blast_db/nt.nal'    
   
 
 
 # echo '['; find | grep fastq | cut -c1-35 | sort | uniq | while read x; do x=`basename "$x"`; echo "('`pwd`/${x}1.fastq', '`pwd`/${x}2.fastq'),"; done; echo ']'
-  samples = [
-('/data/tmp/marchulsman/data_feng/C1V35ACXX_102179-01_ATCACG_L003_R1.fastq', '/data/tmp/marchulsman/data_feng/C1V35ACXX_102179-01_ATCACG_L003_R2.fastq'),
-('/data/tmp/marchulsman/data_feng/C1V35ACXX_102179-02_CGATGT_L003_R1.fastq', '/data/tmp/marchulsman/data_feng/C1V35ACXX_102179-02_CGATGT_L003_R2.fastq'),
-('/data/tmp/marchulsman/data_feng/C1V35ACXX_102179-03_TTAGGC_L003_R1.fastq', '/data/tmp/marchulsman/data_feng/C1V35ACXX_102179-03_TTAGGC_L003_R2.fastq'),
-('/data/tmp/marchulsman/data_feng/C1V35ACXX_102179-04_TGACCA_L003_R1.fastq', '/data/tmp/marchulsman/data_feng/C1V35ACXX_102179-04_TGACCA_L003_R2.fastq'),
-('/data/tmp/marchulsman/data_feng/C1V35ACXX_102179-05_ACAGTG_L003_R1.fastq', '/data/tmp/marchulsman/data_feng/C1V35ACXX_102179-05_ACAGTG_L003_R2.fastq'),
-('/data/tmp/marchulsman/data_feng/C1V35ACXX_102179-06_GCCAAT_L003_R1.fastq', '/data/tmp/marchulsman/data_feng/C1V35ACXX_102179-06_GCCAAT_L003_R2.fastq'),
-('/data/tmp/marchulsman/data_feng/C1V35ACXX_102179-07_CAGATC_L003_R1.fastq', '/data/tmp/marchulsman/data_feng/C1V35ACXX_102179-07_CAGATC_L003_R2.fastq'),
-('/data/tmp/marchulsman/data_feng/C1V35ACXX_102179-08_ACTTGA_L003_R1.fastq', '/data/tmp/marchulsman/data_feng/C1V35ACXX_102179-08_ACTTGA_L003_R2.fastq'),
-('/data/tmp/marchulsman/data_feng/C1V35ACXX_102179-09_GATCAG_L003_R1.fastq', '/data/tmp/marchulsman/data_feng/C1V35ACXX_102179-09_GATCAG_L003_R2.fastq'),
-('/data/tmp/marchulsman/data_feng/C1V35ACXX_102179-10_TAGCTT_L003_R1.fastq', '/data/tmp/marchulsman/data_feng/C1V35ACXX_102179-10_TAGCTT_L003_R2.fastq'),
-('/data/tmp/marchulsman/data_feng/C1V35ACXX_102179-11_GGCTAC_L003_R1.fastq', '/data/tmp/marchulsman/data_feng/C1V35ACXX_102179-11_GGCTAC_L003_R2.fastq'),
-('/data/tmp/marchulsman/data_feng/C1V35ACXX_102179-12_CTTGTA_L003_R1.fastq', '/data/tmp/marchulsman/data_feng/C1V35ACXX_102179-12_CTTGTA_L003_R2.fastq'),
-('/data/tmp/marchulsman/data_feng/C1V35ACXX_102179-13_AGTCAA_L003_R1.fastq', '/data/tmp/marchulsman/data_feng/C1V35ACXX_102179-13_AGTCAA_L003_R2.fastq'),
-('/data/tmp/marchulsman/data_feng/C1V35ACXX_102179-14_AGTTCC_L003_R1.fastq', '/data/tmp/marchulsman/data_feng/C1V35ACXX_102179-14_AGTTCC_L003_R2.fastq'),
-('/data/tmp/marchulsman/data_feng/C1V35ACXX_102179-15_ATGTCA_L003_R1.fastq', '/data/tmp/marchulsman/data_feng/C1V35ACXX_102179-15_ATGTCA_L003_R2.fastq'),
-('/data/tmp/marchulsman/data_feng/C1V35ACXX_102179-16_CCGTCC_L003_R1.fastq', '/data/tmp/marchulsman/data_feng/C1V35ACXX_102179-16_CCGTCC_L003_R2.fastq'),
-('/data/tmp/marchulsman/data_feng/C1V35ACXX_102179-17_GTCCGC_L003_R1.fastq', '/data/tmp/marchulsman/data_feng/C1V35ACXX_102179-17_GTCCGC_L003_R2.fastq'),
-('/data/tmp/marchulsman/data_feng/C1V35ACXX_102179-18_GTGAAA_L003_R1.fastq', '/data/tmp/marchulsman/data_feng/C1V35ACXX_102179-18_GTGAAA_L003_R2.fastq'),
+[
+('/data/tmp/marchulsman/data_rj/102020-01_R1.fastq1.fastq', '/data/tmp/marchulsman/data_rj/102020-01_R1.fastq2.fastq'),
+('/data/tmp/marchulsman/data_rj/102020-01_R2.fastq1.fastq', '/data/tmp/marchulsman/data_rj/102020-01_R2.fastq2.fastq'),
+('/data/tmp/marchulsman/data_rj/102020-02_R1.fastq1.fastq', '/data/tmp/marchulsman/data_rj/102020-02_R1.fastq2.fastq'),
+('/data/tmp/marchulsman/data_rj/102020-02_R2.fastq1.fastq', '/data/tmp/marchulsman/data_rj/102020-02_R2.fastq2.fastq'),
+('/data/tmp/marchulsman/data_rj/102020-03_R1.fastq1.fastq', '/data/tmp/marchulsman/data_rj/102020-03_R1.fastq2.fastq'),
+('/data/tmp/marchulsman/data_rj/102020-03_R2.fastq1.fastq', '/data/tmp/marchulsman/data_rj/102020-03_R2.fastq2.fastq'),
+('/data/tmp/marchulsman/data_rj/102020-04_R1.fastq1.fastq', '/data/tmp/marchulsman/data_rj/102020-04_R1.fastq2.fastq'),
+('/data/tmp/marchulsman/data_rj/102020-04_R2.fastq1.fastq', '/data/tmp/marchulsman/data_rj/102020-04_R2.fastq2.fastq'),
+('/data/tmp/marchulsman/data_rj/102020-05_R1.fastq1.fastq', '/data/tmp/marchulsman/data_rj/102020-05_R1.fastq2.fastq'),
+('/data/tmp/marchulsman/data_rj/102020-05_R2.fastq1.fastq', '/data/tmp/marchulsman/data_rj/102020-05_R2.fastq2.fastq'),
+('/data/tmp/marchulsman/data_rj/102020-06_R1.fastq1.fastq', '/data/tmp/marchulsman/data_rj/102020-06_R1.fastq2.fastq'),
+('/data/tmp/marchulsman/data_rj/102020-06_R2.fastq1.fastq', '/data/tmp/marchulsman/data_rj/102020-06_R2.fastq2.fastq'),
+('/data/tmp/marchulsman/data_rj/102020-07_R1.fastq1.fastq', '/data/tmp/marchulsman/data_rj/102020-07_R1.fastq2.fastq'),
+('/data/tmp/marchulsman/data_rj/102020-07_R2.fastq1.fastq', '/data/tmp/marchulsman/data_rj/102020-07_R2.fastq2.fastq'),
+('/data/tmp/marchulsman/data_rj/102020-09_R1.fastq1.fastq', '/data/tmp/marchulsman/data_rj/102020-09_R1.fastq2.fastq'),
+('/data/tmp/marchulsman/data_rj/102020-09_R2.fastq1.fastq', '/data/tmp/marchulsman/data_rj/102020-09_R2.fastq2.fastq'),
+('/data/tmp/marchulsman/data_rj/102020-10_R1.fastq1.fastq', '/data/tmp/marchulsman/data_rj/102020-10_R1.fastq2.fastq'),
+('/data/tmp/marchulsman/data_rj/102020-10_R2.fastq1.fastq', '/data/tmp/marchulsman/data_rj/102020-10_R2.fastq2.fastq'),
+('/data/tmp/marchulsman/data_rj/102020-12_R1.fastq1.fastq', '/data/tmp/marchulsman/data_rj/102020-12_R1.fastq2.fastq'),
+('/data/tmp/marchulsman/data_rj/102020-12_R2.fastq1.fastq', '/data/tmp/marchulsman/data_rj/102020-12_R2.fastq2.fastq'),
+('/data/tmp/marchulsman/data_rj/102020-13_R1.fastq1.fastq', '/data/tmp/marchulsman/data_rj/102020-13_R1.fastq2.fastq'),
+('/data/tmp/marchulsman/data_rj/102020-13_R2.fastq1.fastq', '/data/tmp/marchulsman/data_rj/102020-13_R2.fastq2.fastq'),
+('/data/tmp/marchulsman/data_rj/102020-14_R1.fastq1.fastq', '/data/tmp/marchulsman/data_rj/102020-14_R1.fastq2.fastq'),
+('/data/tmp/marchulsman/data_rj/102020-14_R2.fastq1.fastq', '/data/tmp/marchulsman/data_rj/102020-14_R2.fastq2.fastq'),
+('/data/tmp/marchulsman/data_rj/102020-17_R1.fastq1.fastq', '/data/tmp/marchulsman/data_rj/102020-17_R1.fastq2.fastq'),
+('/data/tmp/marchulsman/data_rj/102020-17_R2.fastq1.fastq', '/data/tmp/marchulsman/data_rj/102020-17_R2.fastq2.fastq'),
+('/data/tmp/marchulsman/data_rj/102020-19_R1.fastq1.fastq', '/data/tmp/marchulsman/data_rj/102020-19_R1.fastq2.fastq'),
+('/data/tmp/marchulsman/data_rj/102020-19_R2.fastq1.fastq', '/data/tmp/marchulsman/data_rj/102020-19_R2.fastq2.fastq'),
+('/data/tmp/marchulsman/data_rj/102020-22_R1.fastq1.fastq', '/data/tmp/marchulsman/data_rj/102020-22_R1.fastq2.fastq'),
+('/data/tmp/marchulsman/data_rj/102020-22_R2.fastq1.fastq', '/data/tmp/marchulsman/data_rj/102020-22_R2.fastq2.fastq'),
+('/data/tmp/marchulsman/data_rj/102020-23_R1.fastq1.fastq', '/data/tmp/marchulsman/data_rj/102020-23_R1.fastq2.fastq'),
+('/data/tmp/marchulsman/data_rj/102020-23_R2.fastq1.fastq', '/data/tmp/marchulsman/data_rj/102020-23_R2.fastq2.fastq'),
+('/data/tmp/marchulsman/data_rj/102020-24_R1.fastq1.fastq', '/data/tmp/marchulsman/data_rj/102020-24_R1.fastq2.fastq'),
+('/data/tmp/marchulsman/data_rj/102020-24_R2.fastq1.fastq', '/data/tmp/marchulsman/data_rj/102020-24_R2.fastq2.fastq'),
+('/data/tmp/marchulsman/data_rj/102020-28_R1.fastq1.fastq', '/data/tmp/marchulsman/data_rj/102020-28_R1.fastq2.fastq'),
+('/data/tmp/marchulsman/data_rj/102020-28_R2.fastq1.fastq', '/data/tmp/marchulsman/data_rj/102020-28_R2.fastq2.fastq'),
 ]
-  sample_names = [ "F1_a","F3_a", "F5_a", "F1_b", "F3_b", "F5_b", "F1_c","F3_c","F5_c","N1_a","N3_a", "N5_a","N1_b","N3_b","N5_b", "N1_c","N3_c","N5_c"];
-  
-  sample_labels = [ 0, 1, 2, 0, 1, 2, 0, 1, 2, 3, 4, 5, 3, 4, 5, 3, 4, 5];
-  label_names   = [ "F1", "F3", "F5", "N1", "N3", "N5" ];
+
+  sample_names =  ["n402_d3_r3","dhex_d7_r3","dhex_d3_r1","dhex_d7_r1","n402_d5_r1","n402_d5_r3","n402_d7_r3","n402_d9_r3","n402_d3_r2","n402_d7_r1","dhex_d3_r2","dhex_d7_r2","n402_d5_r2","n402_d3_r1","n402_d7_r2","n402_d9_r2","n402_d9_r1","dhex_d3_r3"]
+  sample_labels = [0,            5,           4,           5,          1,            1,           2,           3,           0,           2,          4,            5,           1,           0,           2,           3,           3,           4]
+  label_names   = [ "n402_d3", "n402_d5", "n402_d7", "n402_d9", "dhex_d3", "dhex_d7" ];
 
   genome = '/tudelft.net/staff-groups/ewi/insy/DBL/marchulsman/projects/n402_sequence/assembly/n402_atcc.unpadded.fasta' 
   genome_annot = '/tudelft.net/staff-groups/ewi/insy/DBL/marchulsman/projects/n402_sequence/annotations/results/n402_annotations.gff'
-
 
   annots_file = '/tudelft.net/staff-groups/ewi/insy/DBL/marchulsman/projects/n402_sequence/annotation_data/annots_v2.dat'
 
