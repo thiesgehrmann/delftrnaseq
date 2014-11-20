6c86c89fd78420065d360f179d6ccc152797cbbf
Thies Gehrmann <t.gehrmann@tudelft.nl>
2014-11-20 13:05:33 +0100

    minor change

--- pipeline_isoform_dense_cufflinks_rabt.py
+++ pipeline_isoform_dense_cufflinks_rabt.py
@@ -5,7 +5,7 @@ C = init_conf()
 
 cmds = [];
 
-BAM = cor(C.__isoform_dense_star_align_output_bam__);
+BAM = cor(C.__isoform_dense_star_align_output_merged__);
 split_gff, split_fasta = cor(C.__isoform_dense_genome_split_output__);
 
 cuff_outdir  = cor(C.__isoform_dense_cufflinks_outdir__)
@@ -17,7 +17,7 @@ attr_name = cor(C.__isoform_dense_attr_name__);
 cmds.append("mkdir -p %s" % cuff_outdir);
 
 cmds.append(("cufflinks -o %s " % cuff_outdir) + \
-            ("%s " % cuffopts + \
+            ("%s " % cuffopts ) + \
             ("-g %s " % split_gff) +\
             ("-v ") + \
             ("-b %s " % C.cufflinks_bias_corr if C.cufflinks_bias_corr else "") + \
@@ -26,7 +26,7 @@ cmds.append(("cufflinks -o %s " % cuff_outdir) + \
 cmds.append("gffread -EF %s -o %s" % (cuff_outputs[2], cuff_outputs[-2]));
 
 
-cmds.append("%s/utilities/split_genome.py setx2y %s %s attribute seqname name %s" % (C.inst_loc, cuff_outputs[-2], attr_name, cuff_outputs[-1]);
+cmds.append("%s/utilities/split_genome.py setx2y %s %s attribute seqname name %s" % (C.inst_loc, cuff_outputs[-2], attr_name, cuff_outputs[-1]));
 
 
 sys.exit(run_seq_cmds(cmds));
